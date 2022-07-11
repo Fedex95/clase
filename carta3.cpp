e6140ae9975e53ba0d6e66834d93e8769167baf0
Fedex95 <fedex95@outlook.es>
2022-07-11 12:54:53 -0500

    cartaFedex

---
+++ carta3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void corazonrojo()
+{
+    cout<<"╔═════════╗"<<endl;
+    cout<<"║3   ♥    ║"<<endl;
+    cout<<"║         ║"<<endl;
+    cout<<"║    ♥    ║"<<endl;
+    cout<<"║         ║"<<endl;
+    cout<<"║    ♥  3 ║"<<endl;
+    cout<<"╚═════════╝"<<endl;
+}
+
+void diamante(int carta2)
+{
+    cout<<"╔═════════╗"<<endl;
+    cout<<"║3   ♦    ║"<<endl;
+    cout<<"║         ║"<<endl;
+    cout<<"║    ♦    ║"<<endl;
+    cout<<"║         ║"<<endl;
+    cout<<"║    ♦  3 ║"<<endl;
+    cout<<"╚═════════╝";
+}
+
+int main ()
+{
+    corazonrojo;
+    diamante;
+
+    return 0; 
+}
